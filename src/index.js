ba562dc9d2e1ae2ec9a7a0e51dc564744f056aa0
nidhiambatkar026 <nidhiambatkar@gmail.com>
2025-03-10 19:27:40 +0530

    routing logic added using javascript

--- src/index.js
+++ src/index.js
@@ -3,6 +3,23 @@ import { createRoot } from "react-dom/client";
 function App(){
     const path = window.location.pathname;
     console.log(path);
+
+    if(path == "/"){
+        root.render(<Home />)
+    }
+    else if (path == "/dashboard")
+    {
+        root.render(<Dashboard />)
+    }
+    else if (path == "/about"){
+        root.render(<About />)
+    }
+    else if (path == "/contact"){
+        root.render(<Contact />)
+    }
+    else{
+        root.render(<h1>404 Page not found</h1>)
+    }
 }
 
 const root = createRoot(document.getElementById("root"));
